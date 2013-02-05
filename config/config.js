fa5000dc867b750aaf8687fa1b698a1ee437a25a
jiliang <jiliang@yahoo-inc.com>
2013-02-05 17:36:23 +0800

    add lib scaner

--- config/config.js
+++ config/config.js
@@ -23,6 +23,18 @@ config.defaultAppSeed = "http://yui.yahooapis.com/3.6.0/build/yui/yui-min.js";
 config.testSeed = config.arrowModuleRoot + "lib/client/yuitest-seed.js";
 config.testRunner = config.arrowModuleRoot + "lib/client/yuitest-runner.js";
 config.autolib = config.arrowModuleRoot + "lib/common";
+
+// config for share lib
+config.arrowModuleRoot=__dirname+'../../';
+config.defaultShareLibPath=config.arrowModuleRoot+"../";
+config.shareLibMetaPath=config.arrowModuleRoot+"tmp/";
+
+config.ignoreScan=['arrow','Arrow','yahoo-arrow','yui'];
+config.registeDir=["server","client","common"];
+config.clientConfigName="client_seed.js";
+config.serverConfigName="server_seed.js";
+
+
 config.descriptorName = "test_descriptor.json";
 config.minPort = 10000;
 config.maxPort = 11000;
