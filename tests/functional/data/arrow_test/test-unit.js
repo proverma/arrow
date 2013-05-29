79f88f6559d0b9539dc1b45b035589fa9d4adec2
jiliang <jiliang@yahoo-inc.com>
2013-05-29 11:22:44 +0800

    fix bug 6307453

--- tests/functional/data/arrow_test/test-unit.js
+++ tests/functional/data/arrow_test/test-unit.js
@@ -1,5 +1,5 @@
 
-YUI.add("media-greeter-tests", function (Y) {
+YUI.add("media-greeter1-tests", function (Y) {
     //Create a basic test suite
     //We're calling it "unit test suite"
     var suite = new Y.Test.Suite("unit test suite");
@@ -18,4 +18,30 @@ YUI.add("media-greeter-tests", function (Y) {
     //Note we are not "running" the suite. 
     //Arrow will take care of that. We simply need to "add" it to the runner
     Y.Test.Runner.add(suite);
+    Y.Test.Runner.run();
+    console.log('test 1 runner started already');
+}, "0.1", {requires:["test","media-greeter"]});
+
+
+YUI.add("media-greeter2-tests", function (Y) {
+    //Create a basic test suite
+    //We're calling it "unit test suite"
+    var suite = new Y.Test.Suite("unit test suite2");
+
+    //Add a test case to the suite; "test greet"
+    suite.add(new Y.Test.Case({
+        "test greet": function() {
+            var greeter = new Y.Media.Greeter();
+
+            //The method we are testing will inverse the firstname and lastname
+            //Our test   will check for that inversion
+            Y.Assert.areEqual(greeter.greet("Will", "Smith"), "Smith, Will");
+        }
+    }));
+
+    //Note we are not "running" the suite.
+    //Arrow will take care of that. We simply need to "add" it to the runner
+    Y.Test.Runner.add(suite);
+    Y.Test.Runner.run();
+    console.log('test 2 runner started already');
 }, "0.1", {requires:["test","media-greeter"]});
