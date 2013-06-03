a5f454616057f87d2dd240a1e6803d8c26e1aea8
Jun Wang <junwang@yahoo-inc.com>
2013-06-03 11:23:55 +0800

    use mock-child-process.js to inject header

--- sharelib/lib/server/istanbul-command-runner.js
+++ sharelib/lib/server/istanbul-command-runner.js
@@ -12,12 +12,14 @@ YUI.add("istanbul-command-runner", function (Y) {
         counter = 0,
         path = require('path'),
         coverage_dir,
+        injected_file_prefix,
         istanbul_bin = path.resolve(__dirname, "../../../node_modules/istanbul", 'lib', 'cli.js'),
         istanbul_exclude_pattern,
         istanbul_root;
 
     // hardcode the directory of child process coverage report, it has to match with glob in lib/session/sessionfactory.js
     coverage_dir = "child_process_coverage";
+    injected_file_prefix = "temp-for-coverage-";
 
     Y.IstanbulCommandRunner = {
 
@@ -43,8 +45,63 @@ YUI.add("istanbul-command-runner", function (Y) {
          * @param pattern the exclude pattern for istanbul
          *
          */
-        set_exclude_pattern: function (pattern) {
+        setExcludePattern: function (pattern) {
             istanbul_exclude_pattern = pattern;
+        },
+
+        /**
+         * Inject the header of mocking child_process to JS file
+         * So that its child_process can also be instrumented and collected coverage, recursively.
+         *
+         * @param file the JS to be injected header
+         *
+         * @return injected file (path)
+         */
+        injectMockeryHeader: function (file) {
+            var dirname = path.dirname(file),
+                basename = path.basename(file),
+                fs = require('fs'),
+                new_file,
+                orig_content,
+                mocker_module,
+                lines,
+                header;
+
+            new_file = path.resolve(dirname, injected_file_prefix + basename);
+//
+            header = "var mockery = require('mockery');\
+                    var YUI = require('yui').YUI;\
+                    var Y = YUI({ useSync: true, modules: { 'istanbul-command-runner': { fullpath: \"" + __filename + "\" } } }).use('istanbul-command-runner');\
+                    var mock_child_process = { spawn: Y.IstanbulCommandRunner.spawn };\
+                    Y.IstanbulCommandRunner.setIstanbulRoot('" + istanbul_root + "');\
+                    Y.IstanbulCommandRunner.setExcludePattern('" + istanbul_exclude_pattern + "');\
+                    mockery.registerMock('child_process', mock_child_process);\
+                    mockery.enable({ useCleanCache: true, warnOnReplace: false, warnOnUnregistered: false });";
+//
+//
+            mocker_module = __dirname + "/mock-child-process.js"
+            header = "var mockery = require('mockery');\
+                    var path = require('path');\
+                    var mocker = require('" + mocker_module + "');\
+                    var mock_child_process = { spawn: mocker.spawn };\
+                    mocker.set_istanbul_root('" + istanbul_root + "');\
+                    mocker.set_exclude_pattern('" + istanbul_exclude_pattern + "');\
+                    mockery.registerMock('child_process', mock_child_process);\
+                    mockery.enable({ useCleanCache: true, warnOnReplace: false, warnOnUnregistered: false });";
+//
+            orig_content = fs.readFileSync(file, 'utf8');
+
+            //check if the first line is shebang interpreter directive
+            lines = orig_content.split("\n");
+            if (lines[0].indexOf("#!") === 0) {
+                lines[0] += "\n" + header;
+            } else {
+                lines[0] = header + lines[0];
+            }
+
+            fs.writeFileSync(new_file, lines.join("\n"));
+
+            return new_file;
         },
 
         /**
@@ -59,7 +116,7 @@ YUI.add("istanbul-command-runner", function (Y) {
         spawn: function (file, args, options) {
             counter += 1;
 
-            var cmd = '/home/y/bin/node',
+            var cmd = 'node',
                 realArgs = [
                     istanbul_bin,
                     'cover',
@@ -68,6 +125,7 @@ YUI.add("istanbul-command-runner", function (Y) {
                     '-x=' + istanbul_exclude_pattern,
                     '--dir=' + path.resolve(coverage_dir, process.pid + '.' + counter)
                 ],
+                self = this,
                 injected_file,
                 cp;
 
@@ -78,8 +136,7 @@ YUI.add("istanbul-command-runner", function (Y) {
                 realArgs.push('-v');
             }
 
-            //injected_file = inject_mockery_header(file);
-            injected_file = file;
+            injected_file = self.injectMockeryHeader(file);
 
             realArgs.push(injected_file);
 
@@ -94,53 +151,8 @@ YUI.add("istanbul-command-runner", function (Y) {
             }
             cp = require('child_process').spawn(cmd, realArgs, options);
             return cp;
-        },
-
-        /**
-         * run the istanbul cover command, passing it the real command to run
-         *
-         * @method runCommand 
-         *
-         * @param {String} jsFile: the path of JS file to be runned in a spawned child process
-         * @param {Array} iargs: the extra arguments pass to istanbul cover command
-         * @param {Array} args: the arguments pass to JS file execution 
-         * @param {Object} opts: the options pass to child_process.spawn
-         *
-         * @return spawned ChildProcess object
-         */
-        runCommand: function (jsFile, iargs, args, opts) {
-            counter += 1;
-            var cmd = '/home/y/bin/node',
-                realArgs = [
-                    istanbul_bin,
-                    'cover',
-                    '--report=lcov',
-                    '--dir=' + path.resolve(coverage_dir, 'c' + counter)
-                ],
-                cp;
-
-            // get the extra args for istanbul command
-            realArgs.push.apply(realArgs, iargs);
-            opts = opts || {};
-
-            if (verbose) {
-                realArgs.push('-v');
-            }
-            realArgs.push(jsFile);
-
-            // get the extra args for JS file 
-            if (args) {
-                realArgs.push('--');
-                realArgs.push.apply(realArgs, args);
-            }
-
-            if (verbose) {
-                console.log('Run:' + cmd + ' ' + realArgs.join(' '));
-            }
-            cp = spawn(cmd, realArgs, opts);
-            return cp;
         }
     };
 }, "0.1", {
-    requires: ["test"]
+    requires: [""]
 });
