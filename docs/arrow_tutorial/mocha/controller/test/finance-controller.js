53ac340ea305e74b94128449caaaf9125ee949fc
Pranav Parikh <pranav.r.parikh@gmail.com>
2013-12-14 20:11:27 -0800

    Adding tutorial examples for mocha

---
+++ docs/arrow_tutorial/mocha/controller/test/finance-controller.js
@@ -0,0 +1,69 @@
+/*jslint forin:true sub:true anon:true, sloppy:true, stupid:true nomen:true, node:true continue:true*/
+
+/*
+ * Copyright (c) 2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+
+var util = require("util");
+var log4js = require("arrow").log4js;
+var Controller = require("arrow").controller;
+
+function FinanceCustomController(testConfig,args,driver) {
+    Controller.call(this, testConfig,args,driver);
+
+    this.logger = log4js.getLogger("FinanceCustomController");
+}
+
+/*
+ * All controllers MUST implement the Controller interface
+ */
+util.inherits(FinanceCustomController, Controller);
+
+
+/**
+ * In the execute method you get full access to webdriver's methods
+ * Additionally, you can get a handle to the parameters in your descriptor
+ * file by using this.testParams
+ *
+ * Lastly, in this case, the last statement is to execute the test
+ * You'll note executeTest includes the same parameters as Arrow's CLI
+ */
+FinanceCustomController.prototype.execute = function(callback) {
+    var self = this;
+
+    if(this.driver.webdriver){
+
+        //Get the various parameters needed from the Test Descriptor file
+        var txtLocator =  this.testParams.txtLocator;
+        var typeText =  this.testParams.typeText;
+        var btnLocator =  this.testParams.btnLocator;
+        var page = this.testParams.page;
+
+        //Get a handle of the WebDriver Object
+        var webdriver = this.driver.webdriver;
+
+        //Open the page you want to test
+        webdriver.get(page).then(function() {
+            self.logger.info(self.config);
+
+            //Navigate the page as necessary
+            webdriver.findElement(webdriver.By.css(txtLocator)).sendKeys(typeText);
+            webdriver.findElement(webdriver.By.css(btnLocator)).click();
+            self.testParams.page=null;
+            webdriver.getTitle().then(function(title) {
+
+                //Execute the test
+                self.driver.executeTest(self.testConfig, self.testParams, function(error, report) {
+                    callback();
+                });
+            });
+        });
+    }else{
+        this.logger.fatal("Custom Controllers are currently only supported on Selenium Browsers");
+    }
+}
+
+module.exports = FinanceCustomController;
