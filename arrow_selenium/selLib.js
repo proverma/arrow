89426326810520c4261364d884ce6850a8e2a947
Pranav Parikh <pranavp@singcookoff-lm.(none)>
2014-02-04 22:53:36 -0800

    Refactoring for using arrow_selenium as library

--- arrow_selenium/selLib.js
+++ arrow_selenium/selLib.js
@@ -1,15 +1,14 @@
-#!/usr/bin/env node
-
-/*jslint forin:true sub:true anon:true, sloppy:true, stupid:true nomen:true, node:true continue:true*/
+/**jslint forin:true sub:true anon:true, sloppy:true, stupid:true nomen:true, node:true continue:true*/
 
 /*
- * Copyright (c) 2012, Yahoo! Inc.  All rights reserved.
+ * Copyright (c) 2014, Yahoo! Inc. All rights reserved.
  * Copyrights licensed under the New BSD License.
  * See the accompanying LICENSE file for terms.
  */
 
 var fs = require("fs");
 var nopt = require("nopt");
+var util = require("util");
 var log4js = require("log4js");
 
 var Properties = require("../lib/util/properties");
@@ -18,20 +17,45 @@ var ArrowSetup = require('../lib/util/arrowsetup');
 var WdSession = require("../lib/session/wdsession");
 var CapabilityManager = require("../lib/util/capabilitymanager");
 
-
-//getting command line args
-var argv = nopt();
-
 var wd = require("../lib/util/wd-wrapper");
-//setup config
-var prop = new Properties(__dirname + "/../config/config.js", argv.config, argv);
-var config = prop.getAll();
-//console.log(config);
-var logger = log4js.getLogger("selenium");
+var logger = log4js.getLogger("selLib");
 
-var SelLib = require('./SelLib');
+function SelLib(config, argv) {
+    this.config = config;
+    this.argv = argv;
+    this.hub = new WdSession(config);
+    console.log('***SELF ARGS:: ' + JSON.stringify(this.argv));
 
-function listSessions(error, next, arrSessions) {
+}
+
+SelLib.prototype.closeBrowsers = function (sessionCaps, config, args) {
+    var browser,
+        cap,
+        webdriver;
+    for (browser in sessionCaps) {
+        logger.info("Closing browser: " + browser);
+
+        cap = sessionCaps[browser];
+        webdriver =  new wd.Builder().
+            usingServer(config["seleniumHost"]).
+            usingSession(cap.id).
+            build();
+        webdriver.quit();
+    }
+
+};
+
+SelLib.prototype.describeSessions = function (sessionCaps, config, args) {
+    console.log(sessionCaps);
+};
+
+SelLib.prototype.describeSession = function (sessionCap, config, args) {
+    console.log(sessionCap);
+};
+
+
+SelLib.prototype.listSessions = function (error, next, arrSessions, config, argv) {
+
     var sessionCaps = [],
         sessionCount = 0,
         i,
@@ -43,15 +67,16 @@ function listSessions(error, next, arrSessions) {
 start it with: \"java -jar path/to/jar/selenium-server-standalone-<VERSION>.jar\".  Create a browser session on http://127.0.0.1:4444/wd/hub or with \"arrow_server --open=<browser_name>\"\n");
         return;
     }
+
     if (0 === arrSessions.length) {
-        next(sessionCaps);
-    }
+        next(sessionCaps, config, argv);
+    };
 
     function onSessionCap(val) {
         sessionCaps[val.get("browserName")] = val.toJSON();
         sessionCount += 1;
         if (sessionCount === arrSessions.length) {
-            next(sessionCaps);
+            next(sessionCaps, config, argv);
         }
     }
 
@@ -66,16 +91,20 @@ start it with: \"java -jar path/to/jar/selenium-server-standalone-<VERSION>.jar\
         webdriver.getCapabilities().then(function(val) {
             onSessionCap(val)
         });
+
     }
-}
 
-function describeSession(sessionCap) {
-    console.log(sessionCap);
-}
+};
 
+SelLib.prototype.openBrowser = function (sessionCaps, config, argv) {
 
-function openBrowser(sessionCaps) {
-    var browsers = argv.open,
+    var
+        self = this;
+
+//    console.log('\n\n****self in openBrowser::' + util.inspect(self));
+
+    var
+        browsers = argv.open,
         browserList = browsers.split(","),
         webdriver,
         browser,
@@ -83,7 +112,9 @@ function openBrowser(sessionCaps) {
         cm,
         capabilities,
         val;
+
     for (i = 0; i < browserList.length; i += 1) {
+
         browser = browserList[i];
         if (0 === browser.length) { continue; }
 
@@ -95,56 +126,45 @@ function openBrowser(sessionCaps) {
 
         //When user has passed capabilities.json
         if(argv.capabilities){
+
             var caps = {
                 "platform": "ANY",
                 "javascriptEnabled": true,
                 "seleniumProtocol": "WebDriver"
             };
+
             caps.browserName = argv.open;
             if(!caps.browserName){
                 logger.error("No Browser is specified");
                 process.exit(1);
             }
+
             cm = new CapabilityManager();
             capabilities = cm.getCapability(argv.capabilities,caps.browserName);
             if(capabilities === null){
                 logger.error("No related capability for " + caps.browserName + " in " + argv.capabilities);
                 process.exit(1);
             }
-        }
-        else{
-            var capabilities= {
+
+        } else {
+            capabilities= {
                 "browserName": browser,
                 "version": "",
                 "platform": "ANY",
                 "javascriptEnabled": true
             }
         }
+
         webdriver = new wd.Builder().
             usingServer(config["seleniumHost"]).
             withCapabilities(capabilities).build();
-        webdriver.session_.then(describeSession);
+        webdriver.session_.then(self.describeSession);
     }
-}
 
-function closeBrowsers(sessionCaps) {
-    var browser,
-        cap,
-        webdriver;
-    for (browser in sessionCaps) {
-        logger.info("Closing browser: " + browser);
+};
 
-        cap = sessionCaps[browser];
-        webdriver =  new wd.Builder().
-            usingServer(config["seleniumHost"]).
-            usingSession(cap.id).
-            build();
-        webdriver.quit();
-    }
-}
+SelLib.prototype.listHelp = function() {
 
-
-function listHelp() {
     console.info("\nCommandline Options :" + "\n" +
         "--list : Lists all selenium browser sessions" + "\n" +
         "--open=<browser1[, browser2]> : Comma seperated list of browsers to launch" + "\n" +
@@ -156,17 +176,45 @@ function listHelp() {
         "Open Firefox with given capabilities:\n" +
         "arrow_selenium --open=firefox --capabilities=./cap.json\n"
     );
-}
+};
+
+SelLib.prototype.seleniumSessionSetup = function() {
+
+    var self = this,
+        i,
+        sessionId;
+
+//    console.log('\n\n****self in seleniumSessionSetup::' + util.inspect(self));
+
+    if (self.argv.list || self.argv.ls) {
+        self.hub.getSessions(self.describeSessions, self.listSessions, false, self.config, self.argv);
+    }else if (self.argv.open) {
+        console.log('****self.argv::' + JSON.stringify(self.argv));
+//        console.log('****self.listSessions::' + self.listSessions);
+        self.hub.getSessions(self.openBrowser, self.listSessions, true, self.config, self.argv);
+    }else if (self.argv.close) {
+        self.hub.getSessions(self.closeBrowsers, function(error, ref, arrSessions) {
+            if(arrSessions) {
+                logger.info ("Found " + arrSessions.length + " Browsers.")
+                for (i = 0; i < arrSessions.length; i += 1) {
+                    sessionId = arrSessions[i];
+                    logger.info("Killing Session ID :" +sessionId );
+                    var webdriver  = new wd.Builder().
+                        usingServer(self.config["seleniumHost"]).
+                        usingSession(sessionId).
+                        build();
+
+                    webdriver.quit();
+                }
+            }
+        }, self.config, self.argv);
+    } else if (self.argv.help) {
+        self.listHelp();
+    } else {
+        self.listHelp();
+    }
 
 
-var arrowSetup = new ArrowSetup(config, argv);
-arrowSetup.setuplog4js();
-arrowSetup.setupSeleniumHost();
-logger.info("Selenium host: " + config["seleniumHost"]);
+};
 
-var hub = new WdSession(config);
-logger.info("Hub: " + hub);
-var selLib = new SelLib(config,argv);
-logger.info("Selenium host: " + config["seleniumHost"]);
-logger.info("selLib: " + selLib);
-selLib.seleniumSessionSetup();
+module.exports = SelLib;
